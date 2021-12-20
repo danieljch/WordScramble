69ac57e84188272abcbcffd6f866c03b127bedc7
Daniel JCH <danielj.callisayah@gmail.com>
2021-12-20 17:45:00 -0400

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Daniel Jesus Callisaya Hidalgo on 20/12/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
